c97a195c172cee2372cd52b99990c75a7f2911aa
Emanuel Buono <emanuel.9178@gmail.com>
2016-06-15 23:03:33 -0300

    Trabajo practico 3

---
+++ TP3/validations.h
@@ -0,0 +1,7 @@
+#ifndef VALIDATIONS_H_INCLUDED
+#define VALIDATIONS_H_INCLUDED
+
+int getInt(int ,char[],char[], int, int);
+int getStringw(char*,char[],char[], int, int);
+
+#endif // VALIDATIONS_H_INCLUDED
