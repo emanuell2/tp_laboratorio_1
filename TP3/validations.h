6c7899372abfd337bcf768946754f9e3399efb05
Emanuel Buono <emanuel.9178@gmail.com>
2016-06-15 23:05:30 -0300

    No corregir esta version

--- TP3/validations.h
+++
@@ -1,7 +0,0 @@
-#ifndef VALIDATIONS_H_INCLUDED
-#define VALIDATIONS_H_INCLUDED
-
-int getInt(int ,char[],char[], int, int);
-int getStringw(char*,char[],char[], int, int);
-
-#endif // VALIDATIONS_H_INCLUDED
